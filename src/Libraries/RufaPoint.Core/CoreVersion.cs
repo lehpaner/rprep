c0e29ef4ad2592b219a23c4966c116b2a6c52140
unknown <lehpaner@libero.it>
2017-12-27 23:05:03 +0100

    refactoring

---
+++ src/Libraries/RufaPoint.Core/CoreVersion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RufaPoint.Core
+{
+    /// <summary>
+    /// core framework version
+    /// </summary>
+    public static class CoreVersion
+    {
+        /// <summary>
+        /// Gets or sets the store version
+        /// </summary>
+        public static string CurrentVersion
+        {
+            get
+            {
+                return "4.10";
+            }
+        }
+    }
+}
